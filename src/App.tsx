a901982982ada79965bb2484e5d86a24f82a661c
Mishaal Rahman <mishalrm@gmail.com>
2026-09-04 11:32:21 +0400

    selection ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import './App.css'
 import { HeadRibbon } from './components/head-ribbon';
+import malabisHero from './assets/Malabis-Hero.webp';
 
 function App() {
 
   return (
-    <div className=''>
+    <div className='selection:bg-teal-100 selection:text-teal-600'>
       <div className='bg-slate-100 flex flex-col h-screen'>
         <HeadRibbon />
         <div className='flex justify-center items-center pt-8'>
@@ -12,8 +13,8 @@ function App() {
         </div>
         <div className='flex-1 text-slate-900 items-center w-9xl text-[18vh] font-bold flex justify-center tracking-widest'>
           <div className='flex justify-center items-center'>
-            <p className='z-30 '>MALABIS</p>
-            <img src="/Malabis-Hero.webp" alt="" className='z-40 absolute h-7/12 pointer-events-none select-none' />
+            <p className='z-0 '>MALABIS</p>
+            <img src={malabisHero} alt="" className='z-40 absolute h-7/12 pointer-events-none select-none' />
           </div>
         </div>
       </div>
--- src/components/head-ribbon.tsx
+++ src/components/head-ribbon.tsx
@@ -2,7 +2,7 @@
 
 export function HeadRibbon() {
     return (
-        <div className="h-12 max-w-full bg-slate-900 flex justify-center items-center border-b-4 border-green-500">
+        <div className="h-12 max-w-full bg-slate-900 grid place-items-center border-b-4 border-green-500">
             <p className="text-white font-normal tracking-widest text-md">Head Ribbon</p>
         </div>
     );
